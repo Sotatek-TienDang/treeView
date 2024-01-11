c8f75056ab5fa06c4d50fcff6fd2abb90ec16243
tu.nguyen5 <tu.nguyen5@sotatek.com>
2024-01-11 14:45:51 +0700

    create tree view

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,174 @@
+import { TreeView } from "@mui/x-tree-view/TreeView"
+import { TreeItem, treeItemClasses } from "@mui/x-tree-view/TreeItem"
+import { styled } from '@mui/material/styles';
+import { v4 as uuidv4 } from "uuid"
+import { ReactComponent as NoExpand } from "./assets/noExpand.svg"
+import { ReactComponent as Expand } from "./assets/expand.svg"
+import { ReactComponent as Collapse } from "./assets/collapse.svg"
 
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { TreeView } from '@mui/x-tree-view/TreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
+const StyledTreeItem = styled(TreeItem)(() => ({
+  [`& .${treeItemClasses.iconContainer}`]: {
+    '& .close': {
+      opacity: 0.3,
+    },
+  },
+  [`& .${treeItemClasses.group}`]: {
+    marginLeft: 15,
+    paddingLeft: 18,
+    borderLeft: `1px dashed #ffffffde`,
+  },
+}));
 
 
 function App() {
+  const treeData = [
+    {
+      id: "root",
+      name: "Root",
+      children: [
+        {
+          id: uuidv4(),
+          name: "Child - 1",
+          children: [
+            {
+              id: uuidv4(),
+              name: "Child - 1-1",
+              children: [
+                {
+                  id: uuidv4(),
+                  name: "Child - 2-1",
+                },
+                {
+                  id: uuidv4(),
+                  name: "Child - 2-2",
+                  children: [
+                    {
+                      id: uuidv4(),
+                      name: "Child - 2-2-1",
+                    },
+                    {
+                      id: uuidv4(),
+                      name: "Child - 2-2-2",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              id: uuidv4(),
+              name: "Child - 1-2",
+              children: [
+                {
+                  id: uuidv4(),
+                  name: "Child - 2-1",
+                },
+                {
+                  id: uuidv4(),
+                  name: "Child - 2-2",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          id: uuidv4(),
+          name: "Child - 2",
+          children: [
+            {
+              id: uuidv4(),
+              name: "Child - 2-1",
+            },
+            {
+              id: uuidv4(),
+              name: "Child - 2-2",
+            },
+          ],
+        },
+        {
+          id: uuidv4(),
+          name: "Child - 3",
+        },
+      ],
+    },
+  ]
+
+  interface item {
+    id: string
+    name: string
+  }
+  interface Inodes extends item {
+    children?: item[]
+  }
+
+  const renderTree = (nodes: Inodes) => (
+    <StyledTreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </StyledTreeItem>
+  )
 
   return (
     <>
-     <TreeView
-      aria-label="file system navigator"
-      defaultCollapseIcon={<ExpandMoreIcon />}
-      defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-    >
-      <TreeItem nodeId="1" label="Applications">
-        <TreeItem nodeId="2" label="Calendar" />
-      </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="10" label="OSS" />
-        <TreeItem nodeId="6" label="MUI">
-          <TreeItem nodeId="8" label="index.js" />
-        </TreeItem>
-      </TreeItem>
-    </TreeView>
+      <TreeView
+        aria-label="file system navigator"
+        defaultCollapseIcon={<Collapse />}
+        defaultExpandIcon={<Expand />}
+        defaultEndIcon={<NoExpand />}
+        sx={{ flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+      >
+        {treeData.map((treeItem) => renderTree(treeItem))}
+      </TreeView>
     </>
   )
 }
 
 export default App
+
+export const mockData = {
+  version: "1.0.0",
+  expressions: [
+    [
+      {
+        type: "lambda",
+        id: "i_0",
+        body: {
+          type: "lambda",
+          id: "i_1",
+          body: {
+            type: "lambda",
+            id: "i_2",
+            body: {
+              type: "lambda",
+              id: "i_3",
+              body: {
+                type: "variable",
+                id: "i_0",
+              },
+            },
+          },
+        },
+      },
+      {
+        type: "delay",
+        id: "delay",
+        body: {
+          type: "lambda",
+          id: "i_4",
+          body: {
+            type: "variable",
+            id: "i_4",
+          },
+        },
+      },
+    ],
+    {
+      type: "lambda",
+      id: "i_5",
+      body: {
+        type: "variable",
+        id: "i_5",
+      },
+    },
+  ],
+}
